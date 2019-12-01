8ee1af079f4cab831d1e7bf26bddb1a5edf83143
Walter Leibbrandt <git@wrl.co.za>
2019-12-01 08:57:03 +0200

    Add simple undo command: that does git reset --hard HEAD^

---
+++ src/gmx-undo.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+### BOOTSTRAP ###
+set -e
+source $(dirname $(readlink -f $0))/gmx-common.sh
+### /BOOTSTRAP ###
+
+usage() {
+	e_info "Usage: $(basename $0) [<options...>]"
+	e_info
+	e_info "Reverts the last action. Really just 'git reset --hard HEAD^'."
+	e_info
+	e_info "Options:"
+	e_info "-h    Display this message"
+	e_info "-v    Display script version"
+}
+
+### PARSE COMMAND-LINE ARGS ###
+while getopts ":hv" opt
+do
+	case $opt in
+		h)	usage
+			exit 0
+			;;
+		v)	e_info "git-memex version ${GMX_VERSION} -- $(basename $0)"
+			exit 0
+			;;
+		*)	usage
+			e_error "Option does not exist: $OPTARG"
+			exit 1
+			;;
+	esac
+done
+shift $(($OPTIND-1))
+
+if [ $# -gt 1 ]; then
+	usage
+	e_error "Unexpected arguments: $@"
+	exit 1
+fi
+### /PARSE COMMAND-LINE ARGS ###
+
+### MAIN ###
+
+rungit reset --hard HEAD^
+e_success "OK"
